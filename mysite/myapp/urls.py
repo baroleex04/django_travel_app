b1c170e1f118c7c7ed7d434efe687621c0826cec
Le Nguyen Tan Bao <nguyentanbaole2004@gmail.com>
2024-09-06 19:31:13 +0700

    setup the dashboard and liking system

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from myapp.views import *
 
+app_name="myapp"
 urlpatterns = [
     path("", homepage, name=""),
     path("register/", register, name="register"),
     path("login/", my_login, name="login"),
     path("dashboard/", dashboard, name="dashboard"),
     path("logout/", user_logout, name="logout"),
+    path("dashboard/<int:dish_id>", detail, name="detail")
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CreateUserForm, LoginForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import F
 # For authentication
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from .models import *
 
 def homepage(request):
     return render(request, "myapp/index.html")
@@ -28,14 +32,46 @@ def my_login(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 auth.login(request, user)
-                return redirect("dashboard")
+                return redirect("myapp:dashboard")
     context={'loginform':form}
     return render(request, "myapp/login.html", context)
 
 def user_logout(request):
     auth.logout(request)
-    return redirect("")
+    return redirect("myapp:")
 
 @login_required(login_url="login")
 def dashboard(request):
-    return render(request, "myapp/dashboard.html")
+    best_five_dishes = Dish.objects.order_by("-total_like")[:5]
+    context = {
+        "best_five_dishes": best_five_dishes
+    }
+    return render(request, "myapp/dashboard.html", context)
+
+@login_required(login_url="login")
+def detail(request, dish_id):
+    dish = get_object_or_404(Dish, id=dish_id)
+    address = dish.address
+    msg = None
+    if request.user.is_authenticated:
+        if dish.likers.filter(id=request.user.id).exists():
+            msg = "liked"
+    
+    if msg == None:
+        if (request.GET.get('likebtn')):
+            dish.total_like = F("total_like")+1
+            dish.likers.add(request.user)
+            dish.save()
+            return HttpResponseRedirect(reverse("myapp:detail", args=(dish.id,)))
+    else:
+        if (request.GET.get('dislikebtn')):
+            dish.total_like = F("total_like")-1
+            dish.likers.remove(request.user)
+            dish.save()
+            return HttpResponseRedirect(reverse("myapp:detail", args=(dish.id,)))
+    context = {
+        "dish": dish,
+        "address": address,
+        "msg": msg
+    }
+    return render(request, "myapp/detail.html", context)
