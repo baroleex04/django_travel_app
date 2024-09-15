dc248bda496bd2f3f639201de297ea261315a5d2
Le Nguyen Tan Bao <nguyentanbaole2004@gmail.com>
2024-09-15 19:02:57 +0700

    update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -111,4 +111,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-API_KEY = config("API_KEY")
+# API_KEY = config("API_KEY")
