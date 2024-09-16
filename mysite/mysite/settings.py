88ab3120020af90c9935aba46365a3487f13a477
Le Nguyen Tan Bao <nguyentanbaole2004@gmail.com>
2024-09-16 21:17:30 +0700

    change ALLOWED_HOSTS

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-4--slmgrp_pp0r_3e@55gcq$f8xgs#5p9y$lvu%5&b^!np6ns2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '18.138.231.163']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 
 # Application definition
